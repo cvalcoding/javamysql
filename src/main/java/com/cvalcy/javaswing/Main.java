3c40d183588b50c76e709a41608f96b27fe6c3ac
cvalcy <cedric-valcy@hotmail.fr>
2026-06-19 01:18:44 +0200

    First commit

---
+++ src/main/java/com/cvalcy/javaswing/Main.java
@@ -0,0 +1,9 @@
+package com.cvalcy.javaswing;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("first app for java");
+    }
+}
